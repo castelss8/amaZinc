faa450b14407c89fcec0a6acc2ee69905eba4c3c
PieroIlDestriero <dvdbomba@gmail.com>
2024-05-18 11:05:24 +0200

    SAT_Model finito (più o meno)
    Il modello di SAT senza clustering funziona! TODO: File per il clustering + SAT con clustering

--- SAT/SAT_Model.py
+++ SAT/SAT_Model.py
@@ -5,53 +5,27 @@ from itertools import combinations
 from utils import *
 import math
 import time
-
+from objective_function import objective_function
 
 # Def of constraints
+def exactly_k(var: list[BoolRef], k: int):
+    return PbEq([(v,1) for v in var], k)
 
-def at_least_one_seq(bool_vars):
-    return Or(bool_vars)
+def exactly_one(var: list[BoolRef]):
+    return PbEq([(v,1) for v in var], 1)
 
-def at_most_one_seq(bool_vars, name):
-    constraints = []
-    n = len(bool_vars)
-    s = [Bool(f"s_{name}_{i}") for i in range(n - 1)]
-    constraints.append(Or(Not(bool_vars[0]), s[0]))
-    constraints.append(Or(Not(bool_vars[n-1]), Not(s[n-2])))
-    for i in range(1, n - 1):
-        constraints.append(Or(Not(bool_vars[i]), s[i]))
-        constraints.append(Or(Not(bool_vars[i]), Not(s[i-1])))
-        constraints.append(Or(Not(s[i-1]), s[i]))
-    return And(constraints)
+def at_most_k(var: list[BoolRef], k: int):
+    """
+    Return constraint that at most k of the variables in vars are true
+    :param variables: List of variables
+    :param k: Maximum number of variables that can be true
+    :return:
+    """
 
-def exactly_one_seq(bool_vars, name):
-    return And(at_least_one_seq(bool_vars), at_most_one_seq(bool_vars, name))
-
-def at_most_k_seq(bool_vars, k, name):
-    constraints = []
-    n = len(bool_vars)
-    s = [[Bool(f"s_{name}_{i}_{j}") for j in range(k)] for i in range(n - 1)]
-    constraints.append(Or(Not(bool_vars[0]), s[0][0]))
-    constraints += [Not(s[0][j]) for j in range(1, k)]
-    for i in range(1, n-1):
-        constraints.append(Or(Not(bool_vars[i]), s[i][0]))
-        constraints.append(Or(Not(s[i-1][0]), s[i][0]))
-        constraints.append(Or(Not(bool_vars[i]), Not(s[i-1][k-1])))
-        for j in range(1, k):
-            constraints.append(Or(Not(bool_vars[i]), Not(s[i-1][j-1]), s[i][j]))
-            constraints.append(Or(Not(s[i-1][j]), s[i][j]))
-    constraints.append(Or(Not(bool_vars[n-1]), Not(s[n-2][k-1])))   
-    return And(constraints)
-
-def at_least_k_seq(bool_vars, k, name):
-    return at_most_k_seq([Not(var) for var in bool_vars], len(bool_vars)-k, name)
-
-def exactly_k_seq(bool_vars, k, name):
-    return And(at_most_k_seq(bool_vars, k, name+'1'), at_least_k_seq(bool_vars, k, name))
+    return PbLe([(v, 1) for v in var], k)
 
 
-
-def SAT_MCP(n, m, sz, cpt):
+def SAT_MCP(n, m, sz, cpt, D):
     solv= Solver()
     max_w = np.max(cpt)
 
@@ -60,7 +34,7 @@ def SAT_MCP(n, m, sz, cpt):
 
     #Each pack have to be carried by exactly one courrier
     for j in range(n):
-        solv.add(exactly_one_seq([c[i][j] for i in range(m)], f"one_c_for_item({j})"))
+        solv.add(exactly_one([c[i][j] for i in range(m)]))
 
     #The last m coulmns of c are fixed: the courier cour starts and ends at the deposit n+cour
     for j in range(m):
@@ -73,7 +47,7 @@ def SAT_MCP(n, m, sz, cpt):
     #Weight constraint
     for cour in range(m):
         cour_weight = [c[cour][item] for item in range(n) for _ in range(sz[item])]
-        solv.add(at_most_k_seq(cour_weight, cpt[cour]))
+        solv.add(at_most_k(cour_weight, cpt[cour]))
    
     #Create a (n+m)x(n+m) matrix: pred[i][j] = true if the j-th item is the predecessor of the i-th item. 
     #The n+c column in the matrix is the starting point of the c-th courier. The n+c row in the matrix is the ending point of the c-th courier
@@ -85,8 +59,8 @@ def SAT_MCP(n, m, sz, cpt):
         for j in range(n+m):
             col_i += [pred[j][i]]
 
-        solv.add(exactly_one_seq(col_i, f"PC_{i}"))
-        solv.add(exactly_one_seq(pred[i], f"PR_{i}"))
+        solv.add(exactly_one(col_i))
+        solv.add(exactly_one(pred[i]))
 
     #If the courier cour has the item i and the item j is the predecessor of the item i then cour has the item j
     for cour in range(m):
@@ -105,4 +79,63 @@ def SAT_MCP(n, m, sz, cpt):
     for item_i in range(n):
         for item_j in range(n):
             solv.add(Implies(pred[item_i][item_j], Or([  And(no_loops[item_i][k], Not(no_loops[item_j][k])) for k in range(nl_max)  ])  ))
-   
+
+
+    # Time
+    starting_time = time.time()
+    timeout = starting_time + 60*5 #Ending time
+    check_timeout = timeout-time.time() #Time left
+    best_obj = 10000
+
+    #print('Start...')
+
+    while time.time() < timeout:
+        solv.set('timeout', int(check_timeout*1000)) #time left in millisec 
+        solv.push()
+
+        for courier in range(m):
+            tmp_dist = [And(c[courier][item], pred[item][item_2]) for item in range(n) for item_2 in range(n) for _ in range(D[item_2][item])]
+            tmp_dist += [And(c[courier][item], pred[item][n+courier]) for item in range(n) for _ in range(D[n][item])]
+            tmp_dist += [And(c[courier][item], pred[n+courier][item]) for item in range(n) for _ in range(D[item][n])]
+            solv.add(at_most_k(tmp_dist, best_obj-1))
+        
+        if solv.check()==sat:
+            tmp_model = solv.model()
+            item_pred, cour_item = [(i,j) for j in range(n+m) for i in range(n+m) if tmp_model.evaluate(pred[i][j])], [(i, j) for j in range(n) for i in range(m) if tmp_model.evaluate(c[i][j])] #tmp_model.evaluate(c[i][j][0])
+            tmp_obj = objective_function(item_pred, cour_item, n, m, D)
+            if tmp_obj<best_obj:
+                best_solution=tmp_model
+                best_obj=tmp_obj
+                #print(best_obj)
+            check_timeout = timeout-time.time() #Time left
+            solv.pop()
+        else:
+            return best_solution, best_obj
+
+
+
+#TEMPORANEO:
+instance_n=2 #from 1 to 21
+#"C:\Users\mange\Documents\GitHub\Uni\amaZinc\SAT\Instances\inst01.dat"
+if instance_n<10:
+    file_name='inst0'+str(instance_n)+'.dat'
+else:
+    file_name='inst'+str(instance_n)+'.dat'
+file = open('/Users/mange/Documents/GitHub/Uni/amaZinc/SAT/Instances/inst02.dat', 'r')
+#file = open('./Instances/'+file_name, 'r')
+
+splitted_file = file.read().split('\n')
+
+m = int(splitted_file[0])
+n = int(splitted_file[1])
+cpt_tmp = list(map(int, splitted_file[2].split(' ')))
+tmp_sz=splitted_file[3].split(' ')
+if '' in tmp_sz:
+    sz_tmp=list(map(int, [tmp_sz[i] for i in range(len(tmp_sz)) if tmp_sz[i]!='']))
+else:
+    sz_tmp = list(map(int, splitted_file[3].split(' ')))
+D = [list(map(int, line.strip().split(' '))) for line in splitted_file[4:(n+5)]]
+
+print('Instance number '+str(instance_n)+': '+str(n)+' items and '+str(m)+' couriers.')
+SAT_MCP(n, m, sz_tmp, cpt_tmp, D)
+
